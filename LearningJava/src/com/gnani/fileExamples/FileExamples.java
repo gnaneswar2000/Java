e86a0c6c71d9f489a5cc374f68d2f329cb582043
gnane <gnane@Gnaneswar>
2025-04-03 13:27:18 -0400

    Scanner, FileReader, BufferReader Learning.

--- LearningJava/src/com/gnani/fileExamples/FileExamples.java
+++ LearningJava/src/com/gnani/fileExamples/FileExamples.java
@@ -1,22 +1,68 @@
 package com.gnani.fileExamples;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class FileExamples {
 
+	File f = new File("./Learning/Sample.txt");
+
 	public static void main(String[] args) throws IOException {
 		FileExamples fe = new FileExamples();
-		fe.file();
+		// fe.fileInputStream();
+		// fe.scannerExample();
+		// fe.fileReaderExample();
+		fe.bufferReaderExample();
 	}
 
-	public void file() throws IOException {
-		File f = new File("./Learning/Sample.txt");
-		if(!f.exists())
+	public void fileInputStreamExample() throws IOException {
+		if (!f.exists())
 			f.createNewFile();
 		FileInputStream inp = new FileInputStream(f);
-		System.out.println((char)inp.read());
-		inp.close() ;		
+		System.out.println((char) inp.read());
+		inp.close();
+	}
+
+	public void scannerExample() throws FileNotFoundException {
+		Scanner sc = new Scanner(f);
+		String text = "";
+		while (sc.hasNextLine()) {
+			text = sc.nextLine();
+			System.out.println(text);
+		}
+		sc.close();
+	}
+
+	public void fileReaderExample() throws IOException {
+		FileReader fr = new FileReader(f);
+		String text = "";
+		int code;
+		while (fr.read() != -1) {
+			text += (char) fr.read();
+			System.out.print(text);
+		}
+		// System.out.println(text);
+
+		fr.close();
+	}
+
+	public void bufferReaderExample() throws IOException {
+		FileReader fr = new FileReader(f);
+		BufferedReader br = new BufferedReader(fr);
+		String text = new String();
+		String line = new String();
+
+		while ((line = br.readLine()) != null) {
+			text += line + "\n";
+		}
+		System.out.println(text);
+
+		br.close();
+		fr.close();
 	}
 }
