d10adacb5eb07cd0bd5d1fbee1982212fe35c2ef
gnane <gnane@Gnaneswar>
2025-05-19 17:36:44 -0400

    Learing Spring boot. used @primary annotation.

--- LearningSpring/src/main/java/com/learningSpring/Desktop.java
+++ LearningSpring/src/main/java/com/learningSpring/Desktop.java
@@ -1,8 +1,10 @@
 package com.learningSpring;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class Desktop implements Computer{
 
 	public void compile() {
